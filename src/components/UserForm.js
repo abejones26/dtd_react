c005dc61737e4bed2e796718f7e63282f71faae0
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2021-01-12 15:43:34 -0600

    Create a class base component

---
+++ src/components/UserForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        username: '',
+        password: '',
+        email: ''
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UserForm
